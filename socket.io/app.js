ec33b8a134b07c53cb8c0375e412d78e0736fea9
Aditya <adityakale3@gmail.com>
2020-06-17 22:54:41 +0530

    Socket.io Updated

--- socket.io/app.js
+++ socket.io/app.js
@@ -1,28 +1,30 @@
 // https://code.tutsplus.com/tutorials/real-time-sports-application-using-nodejs--cms-30594
 // npm install socket.io
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io').listen(http);
  
-app.get('/', (req,res) => {
-// Check Connection
-    io.on('connection', function(socket){
-        console.log('Client connection received');
-    });
-
-// Send Recieve Socket
-    io.on('connection', function(socket){
-        console.log('Client connection received');
-         
-        socket.emit('sendToClient', { hello: 'world' });
-         
-        socket.on('receivedFromClient', function (data) {
-            console.log(data);
-        });
-    });
-
-
-
+io.on('connection', function(socket){
+    console.log('Client connection received');
 });
+// socket.emit('event', 'For single self user');
+// socket.broadcast.emit('event', 'All except self');
+// io.emit('event', 'Emits to all including self');
+// socket.id // unique ID of current socket;
+// ROOM
+// socket.join('roomName');
+// socket.broadcast.to('roomName').emit('event', 'All except self');
+
+
+app.get('/', (req,res) => {
+    res.render('index');    
+});
+
+
+http.listen(4000, () => {
+console.log('Server Up and Running');
+})
+
 
 
